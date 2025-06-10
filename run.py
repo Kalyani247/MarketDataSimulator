aa90a004bfd305775d40021d3c0be9033f5a5399
Kalyani247 <kalyanicharugundla@gmail.com>
2025-06-10 18:02:46 +0530

    Implemented Market Data Simulator with Kafka producer

--- run.py
+++ run.py
@@ -1,5 +1,11 @@
 from app import initialize_app
+import threading
+from . import simulator  # Import the simulator module from the same directory
 
 app = initialize_app()
-if __name__=='__main__':
-    app.run(debug=True) 
+
+if __name__ == '__main__':
+    simulator_thread = threading.Thread(target=simulator.run_simulator)
+    simulator_thread.daemon = True  # Allow the main program to exit even if the thread is running
+    simulator_thread.start()
+    app.run(debug=True)
